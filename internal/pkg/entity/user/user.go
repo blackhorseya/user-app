dd611bad2d54385c9497b1fe22b6e0f70a454eae
Sean Zheng <blackhorseya@gmail.com>
2022-02-28 16:00:46 +0800

    feat: added user in entity

---
+++ internal/pkg/entity/user/user.go
@@ -0,0 +1,33 @@
+package user
+
+import (
+	"github.com/blackhorseya/user-app/pb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Profile declare a user information
+type Profile struct {
+	ID           primitive.ObjectID `json:"id" bson:"_id"`
+	OpenIds      map[string]string  `json:"open_ids" bson:"open_ids"`
+	Name         string             `json:"name" bson:"name"`
+	Nickname     string             `json:"nickname" bson:"nickname"`
+	Email        string             `json:"email" bson:"email"`
+	Roles        []string           `json:"roles" bson:"roles"`
+	Token        string             `json:"token" bson:"token"`
+	AccessToken  string             `json:"access_token" bson:"access_token"`
+	RefreshToken string             `json:"refresh_token" bson:"refresh_token"`
+	PictureURL   string             `json:"picture_url" bson:"picture_url"`
+	CreatedAt    primitive.DateTime `json:"created_at" bson:"created_at"`
+	UpdatedAt    primitive.DateTime `json:"updated_at" bson:"updated_at"`
+}
+
+// NewProfileResponse new a *pb.Profile form profile entity
+func NewProfileResponse(info *Profile) *pb.Profile {
+	return &pb.Profile{
+		Id:      info.ID.Hex(),
+		Name:    info.Name,
+		Email:   info.Email,
+		Picture: info.PictureURL,
+		Token:   info.Token,
+	}
+}
