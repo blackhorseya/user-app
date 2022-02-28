79d2e09a61179b24f41e5460f303c4da3eea8536
Sean Zheng <blackhorseya@gmail.com>
2022-03-01 06:31:49 +0800

    feat: added auth repo interface

--- internal/app/user/biz/auth/repo/impl.go
+++ internal/app/user/biz/auth/repo/impl.go
@@ -1,6 +1,8 @@
 package repo
 
 import (
+	"github.com/blackhorseya/gocommon/pkg/contextx"
+	"github.com/blackhorseya/user-app/internal/pkg/entity/user"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
 )
@@ -16,4 +18,24 @@ func NewImpl(logger *zap.Logger, client *mongo.Client) IRepo {
 		logger: logger.With(zap.String("type", "auth.repo")),
 		client: client,
 	}
+}
+
+func (i *impl) GetUserByOpenID(ctx contextx.Contextx, provider, id string) (info *user.Profile, err error) {
+	// todo: 2022-03-01|06:26|Sean|impl me
+	panic("implement me")
+}
+
+func (i *impl) GetUserByToken(ctx contextx.Contextx, token string) (info *user.Profile, err error) {
+	// todo: 2022-03-01|06:26|Sean|impl me
+	panic("implement me")
+}
+
+func (i *impl) RegisterUser(ctx contextx.Contextx, newUser *user.Profile) (info *user.Profile, err error) {
+	// todo: 2022-03-01|06:26|Sean|impl me
+	panic("implement me")
+}
+
+func (i *impl) UpdateUser(ctx contextx.Contextx, newUser *user.Profile) (info *user.Profile, err error) {
+	// todo: 2022-03-01|06:26|Sean|impl me
+	panic("implement me")
 }
--- internal/app/user/biz/auth/repo/repo.go
+++ internal/app/user/biz/auth/repo/repo.go
@@ -1,10 +1,21 @@
 package repo
 
-import "github.com/google/wire"
+import (
+	"github.com/blackhorseya/gocommon/pkg/contextx"
+	"github.com/blackhorseya/user-app/internal/pkg/entity/user"
+	"github.com/google/wire"
+)
 
 // IRepo declare auth repository functions
 //go:generate mockery --name=IRepo
 type IRepo interface {
+	GetUserByOpenID(ctx contextx.Contextx, provider, id string) (info *user.Profile, err error)
+
+	GetUserByToken(ctx contextx.Contextx, token string) (info *user.Profile, err error)
+
+	RegisterUser(ctx contextx.Contextx, newUser *user.Profile) (info *user.Profile, err error)
+
+	UpdateUser(ctx contextx.Contextx, newUser *user.Profile) (info *user.Profile, err error)
 }
 
 // ProviderSet is a provider set for wire
